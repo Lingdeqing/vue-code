f9b631ce79ad5e04bcc62e9116c3248763c0caf8
yaolin1 <yaolin1@xiaomi.com>
2019-01-17 14:21:13 +0800

    显性的过渡持续时间

---
+++ src/examples/显性的过度持续时间.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="show = !show">click</button>
+        <transition 
+            enter-active-class="animated tada"
+            leave-active-class="animated bounceOutRight"
+            :duration="{enter: 1000, leave: 800}"
+        >
+            <p v-if="show" class="">hello</p>
+        </transition>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            show: true
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
