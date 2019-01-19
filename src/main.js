dd48082c31237139bb56fc7d3598635935eec68e
yaolin1 <15851853085@163.com>
2019-01-19 11:37:22 +0800

    过度模式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './examples/多个元素的过渡.vue'
+import App from './examples/过度模式.vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
