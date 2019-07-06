6647e8125f5e10e103b31f62469f0c5d5d4f0ff3
姚林 <yaolin1@xiaomi.com>
2019-07-06 09:56:38 +0800

    observable用法

---
+++ src/examples/observable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>
+      {{ globalState.count }}
+      <button @click="globalState.count++">add 1</button>
+    </p>
+    <p>
+      Vue.observable可以使用来观察全局的状态，原来放在vuex中的可以放在Vue.observable中
+    </p>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+const globalState = Vue.observable({
+  count: 1
+});
+export default {
+  data: function() {
+    return {
+      globalState
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './examples/钩子函数循环'
+import App from './examples/observable'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
